86a6d89b398f04ef120a21a0331b7cd011eebcfe
Veronicabrun <105516840+Veronicabrun@users.noreply.github.com>
2025-02-17 20:51:32 +0100

    Removed unused test files example.spec.js and homepage.spec.js

--- tests/e2e/homepage.spec.js
+++
@@ -1,9 +0,0 @@
-//import { test, expect } from "@playwright/test";
-
-//test("homepage should show the News link", async ({ page }) => {
-// Gå til Noroff sin nettside
-//await page.goto("https://www.noroff.no/en");
-
-// Sjekk at "News"-lenken er synlig
-//await expect(page.getByRole("link", { name: "News" })).toBeVisible();
-//});
